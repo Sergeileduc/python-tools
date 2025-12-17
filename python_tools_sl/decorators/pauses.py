c4600ec5b617aab4af503b4d09f9bddd21ec6ed7
Sergeileduc <sergei.leduc@gmail.com>
2025-12-17 22:04:23 +0100

    feat(decorators): :zap: pauses decorators

---
+++ python_tools_sl/decorators/pauses.py
@@ -0,0 +1,27 @@
+import time
+import asyncio
+from functools import wraps
+
+
+def with_pause(seconds=2, message=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(message or f"⏸️ Pause de {seconds}s pour éviter les timeouts...")
+            time.sleep(seconds)
+            return result
+        return wrapper
+    return decorator
+
+
+def with_pause_async(seconds=2, message=None):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            result = await func(*args, **kwargs)
+            print(message or f"⏸️ Pause de {seconds}s pour éviter les timeouts...")
+            await asyncio.sleep(seconds)
+            return result
+        return wrapper
+    return decorator
